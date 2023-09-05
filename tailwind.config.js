a8fc83203dc3ce30a4a50e3802796b1d8f941cc7
Darrell-Rafa <drafa0609@gmail.com>
2023-09-05 23:01:25 +0700

    finish most visited mountain

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,13 +7,6 @@ module.exports = {
       roboto: ["Roboto", "sans-serif"],
       calistoga: ["Calistoga", "cursive"],
     },
-    screens: {
-      sm: "640px",
-      md: "768px",
-      lg: "1024px",
-      xl: "1280px",
-      "2xl": "1536px",
-    },
   },
   plugins: [],
 };
